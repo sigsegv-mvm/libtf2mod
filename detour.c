c7be0037c1fe12fbcced246d035e89228cb5a801
sigsegv <sigsegv@sigpipe.info>
2015-07-25 20:11:08 -0700

    disable robot gib detour by default

--- detour.c
+++ detour.c
@@ -6,7 +6,7 @@ detour_info_t *detours[] = {
 	&detour_minisentry_quickbuild_health_fix,
 	&detour_pushallplayersaway_disable,
 	&detour_robot_tickle_enable,
-	&detour_robot_gib_normal,
+	//&detour_robot_gib_normal,
 	&detour_giantsoldier_crit_sound_fix,
 	&detour_disposable_sentry_enable_health_upgrades,
 	
