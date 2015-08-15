69989e0ecb3c4269b0fef9cf441b13543740d5a5
sigsegv <sigsegv@sigpipe.info>
2015-08-15 16:30:15 -0700

    add entindex info to (In|De)crementTeleporterCount debug

--- detour/wavecounts_debug.c
+++ detour/wavecounts_debug.c
@@ -19,7 +19,7 @@ static unknown_t (*trampoline_CTFObjectiveResource_SetMannVsMachineWaveClassName
 
 static unknown_t detour_CTFObjectiveResource_AddMannVsMachineWaveClassFlags(CTFObjectiveResource* this, int index, unsigned int flags)
 {
-	pr_info("[WaveCount] AddFlags [idx:%d] = %04x\n", index, flags);
+	pr_info("[WaveCount] AddFlags    [idx:%d] = %04x\n", index, flags);
 	return trampoline_CTFObjectiveResource_AddMannVsMachineWaveClassFlags(this, index, flags);
 }
 
@@ -31,50 +31,68 @@ static unknown_t detour_CTFObjectiveResource_ClearMannVsMachineWaveClassFlags(CT
 
 static unknown_t detour_CTFObjectiveResource_DecrementMannVsMachineWaveClassCount(CTFObjectiveResource* this, string_t name, unsigned int flags)
 {
-	pr_warn("[WaveCount] -Class   [name:\"%s\" flags:%04x]\n", name, flags);
+	pr_warn("[WaveCount] -Class      [name:\"%s\" flags:%04x]\n", name, flags);
 	return trampoline_CTFObjectiveResource_DecrementMannVsMachineWaveClassCount(this, name, flags);
 }
 
 static unknown_t detour_CTFObjectiveResource_DecrementTeleporterCount(CTFObjectiveResource* this)
 {
-	pr_warn("[WaveCount] -Teleporter\n");
+	uintptr_t caller = (uintptr_t)__builtin_extract_return_addr(
+		__builtin_return_address(0));
+	uintptr_t caller_frame = (uintptr_t)__builtin_frame_address(1);
+	
+	/* get the caller's CObjectTeleporter* this */
+	CBaseEntity* caller_this = *(CBaseEntity**)(caller_frame + 8);
+	int bldg_entindex = ENTINDEX(caller_this);
+	
+	
+	pr_warn("[WaveCount] -Teleporter [entindex:%d]\n", bldg_entindex);
 	return trampoline_CTFObjectiveResource_DecrementTeleporterCount(this);
 }
 
 static int detour_CTFObjectiveResource_GetMannVsMachineWaveClassCount(CTFObjectiveResource* this, int index)
 {
 	int count = trampoline_CTFObjectiveResource_GetMannVsMachineWaveClassCount(this, index);
-	pr_debug("[WaveCount] GetCount [idx:%d] = %d\n", index, count);
+	pr_debug("[WaveCount] GetCount    [idx:%d] = %d\n", index, count);
 	return count;
 }
 
 static unknown_t detour_CTFObjectiveResource_IncrementMannVsMachineWaveClassCount(CTFObjectiveResource* this, string_t name, unsigned int flags)
 {
-	pr_warn("[WaveCount] +Class   [name:\"%s\" flags:%04x]\n", name, flags);
+	pr_warn("[WaveCount] +Class      [name:\"%s\" flags:%04x]\n", name, flags);
 	return trampoline_CTFObjectiveResource_IncrementMannVsMachineWaveClassCount(this, name, flags);
 }
 
 static unknown_t detour_CTFObjectiveResource_IncrementTeleporterCount(CTFObjectiveResource* this)
 {
-	pr_warn("[WaveCount] +Teleporter\n");
+	uintptr_t caller = (uintptr_t)__builtin_extract_return_addr(
+		__builtin_return_address(0));
+	uintptr_t caller_frame = (uintptr_t)__builtin_frame_address(1);
+	
+	/* get the caller's CObjectTeleporter* this */
+	CBaseEntity* caller_this = *(CBaseEntity**)(caller_frame + 8);
+	int bldg_entindex = ENTINDEX(caller_this);
+	
+	
+	pr_warn("[WaveCount] +Teleporter [entindex:%d]\n", bldg_entindex);
 	return trampoline_CTFObjectiveResource_IncrementTeleporterCount(this);
 }
 
 static unknown_t detour_CTFObjectiveResource_SetMannVsMachineWaveClassActive(CTFObjectiveResource* this, string_t name, bool active)
 {
-	pr_special("[WaveCount] %cActive  [name:\"%s\"]\n", (active ? '+' : '-'), name);
+	pr_special("[WaveCount] %cActive     [name:\"%s\"]\n", (active ? '+' : '-'), name);
 	return trampoline_CTFObjectiveResource_SetMannVsMachineWaveClassActive(this, name, active);
 }
 
 static unknown_t detour_CTFObjectiveResource_SetMannVsMachineWaveClassCount(CTFObjectiveResource* this, int index, int count)
 {
-	pr_info("[WaveCount] SetCount [idx:%d] = %d\n", index, count);
+	pr_info("[WaveCount] SetCount    [idx:%d] = %d\n", index, count);
 	return trampoline_CTFObjectiveResource_SetMannVsMachineWaveClassCount(this, index, count);
 }
 
 static unknown_t detour_CTFObjectiveResource_SetMannVsMachineWaveClassName(CTFObjectiveResource* this, int index, string_t name)
 {
-	pr_special("[WaveCount] SetName  [idx:%d] = \"%s\"\n", index, name);
+	pr_special("[WaveCount] SetName     [idx:%d] = \"%s\"\n", index, name);
 	return trampoline_CTFObjectiveResource_SetMannVsMachineWaveClassName(this, index, name);
 }
 
