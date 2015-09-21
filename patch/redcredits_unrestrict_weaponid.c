c2578ff94b14bda6f19869cd4c38e378b806535d
sigsegv <sigsegv@sigpipe.info>
2015-09-20 21:45:30 -0700

    rewrite red credit weapon ID patch function in C

--- patch/redcredits_unrestrict_weaponid.c
+++ patch/redcredits_unrestrict_weaponid.c
@@ -5,6 +5,16 @@ PATCH(redcredits_unrestrict_weaponid);
 /* allow red credits to drop from revolver headshots */
 
 
+static bool WeaponID_IsSniperRifleOrBowOrRevolver(int id)
+{
+	if (id == TF_WEAPON_REVOLVER) {
+		return true;
+	}
+	
+	return WeaponID_IsSniperRifleOrBow(id);
+}
+
+
 PATCH_CHECK
 {
 	uintptr_t off1 = CALC_RELJMP(CTFPlayer_Event_Killed, 0x08a1,
@@ -26,45 +36,18 @@ PATCH_CHECK
 
 PATCH_APPLY
 {
-	void *new_func = alloc_func(1);
-	
-	
 	uintptr_t off1 = CALC_RELJMP(CTFPlayer_Event_Killed, 0x08a1,
-		new_func);
-	
-	uintptr_t off2 = (uintptr_t)WeaponID_IsSniperRifleOrBow -
-		((uintptr_t)new_func + 0x001b);
+		WeaponID_IsSniperRifleOrBowOrRevolver);
 	
 	
 	size_t data1_base = 0x089c;
 	uint8_t data1[] = {
-		0xe8, CONV_LE(off1) // +089C  call buf
-	};
-	
-	/* wrapper function: return true for 0x2b (revolver weapon ID);
-	 * otherwise, simply call WeaponID_IsSniperRifleOrBow */
-	size_t data2_base = 0x0000;
-	uint8_t data2[] = {
-		0x55,                         // +0000  push ebp
-		0x89, 0xe5,                   // +0001  mov ebp,esp
-		0x83, 0xec, 0x10,             // +0003  sub esp,0x10
-		0xb8, 0x01, 0x00, 0x00, 0x00, // +0006  mov eax,1
-		0x8b, 0x55, 0x08,             // +000B  mov edx,DWORD PTR [ebp+0x8]
-		0x83, 0xfa, 0x2b,             // +000E  cmp edx,0x2b
-		0x74, 0x08,                   // +0011  je +0x8
-		0x89, 0x14, 0x24,             // +0013  mov DWORD PTR [esp],edx
-		0xe8, CONV_LE(off2)           // +0016  call WeaponID_IsSniperRifleOrBow
-		0x89, 0xec,                   // +001B  mov esp,ebp
-		0x5d,                         // +001D  pop ebp
-		0xc3,                         // +001E  ret
+		0xe8, CONV_LE(off1) // +089C  call WeaponID_IsSniperRifleOrBowOrRevolver
 	};
 	
 	
-	/* modify the original function to jump to our new function */
+	/* instead of calling WeaponID_IsSniperRifleOrBow, call our custom function,
+	 * WeaponID_IsSniperRifleOrBowOrRevolver */
 	func_write(CTFPlayer_Event_Killed,
 		data1_base, sizeof(data1), data1);
-	
-	
-	/* write the contents of the new function */
-	memcpy(new_func, data2, sizeof(data2));
 }
