233dca2d0183b38d5bf3eaa9c16b1f1e7375a7e0
sigsegv <sigsegv@sigpipe.info>
2015-11-25 19:25:48 -0800

    add convar for giantsoldier crit sound fix detour

--- detour/giantsoldier_crit_sound_fix.c
+++ detour/giantsoldier_crit_sound_fix.c
@@ -8,16 +8,25 @@ DETOUR(giantsoldier_crit_sound_fix);
 static void (*trampoline_CTFRocketLauncher_Precache)(CTFRocketLauncher* this);
 
 
+static ConVar cvar_sigsegv_giantsoldier_crit_precache_fix;
+
+
 static void detour_CTFRocketLauncher_Precache(CTFRocketLauncher* this)
 {
 	trampoline_CTFRocketLauncher_Precache(this);
 	
-	CBaseEntity_PrecacheScriptSound("MvM.GiantSoldierRocketShootCrit");
+	if (ConVar_GetInt(&cvar_sigsegv_giantsoldier_crit_precache_fix) != 0) {
+		CBaseEntity_PrecacheScriptSound("MvM.GiantSoldierRocketShootCrit");
+	}
 }
 
 
 DETOUR_SETUP
 {
+	ConVar_ctor(&cvar_sigsegv_giantsoldier_crit_precache_fix,
+		"sigsegv_detour_giantsoldier_crit_sound_fix", "0", FCVAR_NOTIFY,
+		"Detour: fix the sound \"MvM.GiantSoldierRocketShootCrit\" not being precached");
+	
 	DETOUR_CREATE(CTFRocketLauncher_Precache);
 	
 	return true;
